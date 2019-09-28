49a972ac5ec4f2eb2eb0e0ef1e6b17ffbf3b3529
Pjotr Prins <pjotr.public01@thebird.nl>
2019-09-28 22:41:53 +0000

    Fetch genome build

--- summary-stats/ebi.rkt
+++ summary-stats/ebi.rkt
@@ -6,18 +6,28 @@
 ;; restarting the process.
 ;;
 
-; (provide list)
+(provide ebi-sumstat-genome-build)
 
 (require json)
 (require net/url)
 (require net/uri-codec)
+(require nested-hash)
 (require memo)
 
-(define (ebi-json query)
-  (call/input-url (string->url (string-append "" (uri-encode query)))
+(define header
+  '("Accept: application/json"))
+
+(define (ebi-sumstat-json query)
+  (call/input-url (string->url (string-append "https://www.ebi.ac.uk/gwas/rest/api/" (uri-encode query)))
                   get-pure-port
                   (lambda (port)
                     (string->jsexpr (port->string port))
                     )
                   header
                   ))
+
+(define (ebi-sumstat-genome-build)
+  (let ([meta (ebi-sumstat-json "metadata")])
+  (define vers (nested-hash-ref meta '_embedded 'mappingMetadatas))
+  (hash-ref (first vers) 'genomeBuildVersion)
+  ))
--- test/ebi-test.rkt
+++ test/ebi-test.rkt
@@ -4,3 +4,9 @@
 (require "../summary-stats/ebi.rkt")
 
 (require rackunit)
+
+(test-case
+ "EBI Summary Statistics Tests"
+ (time (check-equal? (ebi-sumstat-genome-build) "GRCh38.p12"))
+
+ )
