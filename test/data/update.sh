355e9f1922e46022cb5d225b2b589f1110c95d11
Pjotr Prins <pjotr.public01@thebird.nl>
2019-11-02 09:55:30 +0000

    Added test file

--- test/data/update.sh
+++ test/data/update.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-curl 'https://www.ebi.ac.uk/gwas/rest/api/metadata' -i -H 'Accept: application/json' > metadata.json
+curl 'https://www.ebi.ac.uk/gwas/rest/api/metadata' -i -H 'Accept: application/json' > ebi-api-metadata.json
+curl "https://www.ebi.ac.uk/gwas/summary-statistics/api/chromosomes/13/associations?size=10000&bp_lower=32315086&bp_upper=32400266&p_upper=1e-8&p_lower=-0.0" > ebi-sumstats-brca2.json
--- test/ebi-web-test.rkt
+++ test/ebi-web-test.rkt
@@ -12,6 +12,6 @@ This module contains tests that require internet access
 (test-case
  "EBI Summary Statistics Tests (web version)"
  (time (check-equal? (ebi-sumstat-genome-build) "GRCh38.p12"))
-
- (ebi-sumstat-chr-pos-json "13" 32315086 32400266)
+ ; use display to print JSON
+ (display (ebi-sumstat-chr-pos-json "13" 32315086 32400266))
  )
